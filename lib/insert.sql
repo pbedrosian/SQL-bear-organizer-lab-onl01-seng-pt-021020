b42597321e2d6a14c74ff514617b6d3c71e56dc7
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-04-09 00:28:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,3 @@ INSERT INTO bears (name) VALUES ('Mr. Chocolate');
 INSERT INTO bears (name) VALUES ('Mr. Chocolate');
 INSERT INTO bears (name) VALUES ('Mr. Chocolate');
 INSERT INTO bears (name) VALUES ('Mr. Chocolate');
-
